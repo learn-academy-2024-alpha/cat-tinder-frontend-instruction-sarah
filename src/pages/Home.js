1da8f25d2686e3c8c59248af7398e836ea24ce36
Sarah Proctor <sproctor950@gmail.com>
2024-04-02 11:37:19 -0700

    :sparkles: added custom font, header, subheader, images to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,22 @@ import React from "react"
 
 const Home = () => {
   return (
-    <>
-      <h3>Home</h3>
-    </>
+    <div className="page-body">
+      <h1 className="gochi-hand-regular home-title">Cat Tinder</h1>
+      <h3>Find your cat a fury friend!</h3>
+      <div className="home-hero-images">
+        <img
+          src="https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?q=80&w=1736&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="four kittens in a basket"
+          className="home-hero-image"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1511275539165-cc46b1ee89bf?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="gray kitty taking a bath"
+          className="home-hero-image"
+        />
+      </div>
+    </div>
   )
 }
 
