dcbcafa802b42336c007823749d576b82a133177
Sarah Proctor <sproctor950@gmail.com>
2024-04-02 11:17:36 -0700

    :sparkles: adds footer content, navigation, styling, logo asset

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
+import catLogoSmall from "../assets/cat-logo-small.png"
 
 const Footer = () => {
   return (
-    <>
-      <h3>footer</h3>
-    </>
+    <footer>
+      <p>&copy; 2024 Sarah Proctor | LEARN Academy </p>
+      <NavLink to="/">
+        <img
+          src={catLogoSmall}
+          alt="logo of a hand drawn cat face"
+          className="cat-logo-small"
+        />
+      </NavLink>
+    </footer>
   )
 }
 
