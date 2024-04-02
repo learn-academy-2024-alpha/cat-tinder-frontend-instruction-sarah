438b1d79961bc1932a773c612d705f46aca25c65
Sarah Proctor <sproctor950@gmail.com>
2024-04-02 11:41:52 -0700

    :sparkles: adds styling and image asset for not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,20 @@
 import React from "react"
+import smallDog from "../assets/small-dog.jpeg"
 
 const NotFound = () => {
   return (
     <>
-      <h3>Not found</h3>
+      <div className="page-body">
+        <h3 className="gochi-hand-regular home-title">No Cats Here!</h3>
+        <h3>Oops looks like you are in the wrong spot.</h3>
+        <div className="home-hero-images">
+          <img
+            src={smallDog}
+            alt="very cute small dog who is not a cat"
+            className="home-hero-image"
+          />
+        </div>
+      </div>
     </>
   )
 }
