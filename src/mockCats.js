93964d336bfc70dc773cae7ed00681304c2528f9
Sarah Proctor <sproctor950@gmail.com>
2024-04-03 16:44:11 -0700

    :seedling: adds more cats with different size images

--- src/mockCats.js
+++ src/mockCats.js
@@ -13,7 +13,7 @@ const cats = [
     age: 7,
     enjoys: "being queen of the dogs",
     image:
-      "https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      "https://images.unsplash.com/photo-1573865526739-10659fec78a5?q=80&w=3230&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
   },
   {
     id: 3,
@@ -22,6 +22,14 @@ const cats = [
     enjoys: "getting all the attention",
     image:
       "https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+  },
+  {
+    id: 4,
+    name: "Kevin",
+    age: 7,
+    enjoys: "food! And screaming about food!",
+    image:
+      "https://images.unsplash.com/photo-1574231164645-d6f0e8553590?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNhdHN8ZW58MHx8MHx8fDA%3D"
   }
 ]
 
