36d58d234626d7b9b379d8e4cf5d73f03c8377aa
Sarah Proctor <sproctor950@gmail.com>
2024-04-03 14:41:43 -0700

    :sparkles: adds cat index cards and styling

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="cat-index" element={<CatIndex />} />
+        <Route path="cat-index" element={<CatIndex cats={cats} />} />
         <Route path="cat-show" element={<CatShow />} />
         <Route path="cat-new" element={<CatNew />} />
         <Route path="cat-edit" element={<CatEdit />} />
--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -1,10 +1,29 @@
 import React from "react"
+import { Card, CardBody, CardTitle, Button } from "reactstrap"
 
-const CatIndex = () => {
+const CatIndex = ({ cats }) => {
   return (
-    <>
-      <h3>Cat Index</h3>
-    </>
+    <div className="page-body">
+      <h2 className="gochi-hand-regular">Meet All The Cats</h2>
+      <div className="index-cat-cards">
+        {cats.map((cat) => {
+          return (
+            <Card
+              key={cat.id}
+              style={{
+                width: "18rem"
+              }}
+            >
+              <img alt={`profile of ${cat.name}`} src={cat.image} />
+              <CardBody>
+                <CardTitle tag="h5">{cat.name}</CardTitle>
+                <Button>See More Info</Button>
+              </CardBody>
+            </Card>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
