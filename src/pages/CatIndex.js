9b163d6d7f38f73967a799710186d5ddf97a2574
Sarah Proctor <sproctor950@gmail.com>
2024-04-03 16:47:01 -0700

    :art: extracted out the nav button functionality into its own component

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -1,26 +1,28 @@
 import React from "react"
-import { Card, CardBody, CardTitle, Button } from "reactstrap"
-import { Link } from "react-router-dom"
+import { Card, CardBody, CardTitle } from "reactstrap"
+import NavButton from "../components/NavButton"
 
 const CatIndex = ({ cats }) => {
   return (
     <div className="page-body">
-      <h2 className="gochi-hand-regular">Meet All The Cats</h2>
+      <h2 className="gochi-hand-regular page-title">Meet All the Cats</h2>
       <div className="index-cat-cards">
         {cats.map((cat) => {
           return (
-            <Card
-              key={cat.id}
-              style={{
-                width: "18rem"
-              }}
-            >
-              <img alt={`profile of ${cat.name}`} src={cat.image} />
+            <Card key={cat.id}>
+              <div className="index-profile-image-container">
+                <img
+                  alt={`profile of ${cat.name}`}
+                  src={cat.image}
+                  className="index-profile-image"
+                />
+              </div>
               <CardBody>
                 <CardTitle tag="h5">{cat.name}</CardTitle>
-                <Link to={`/cat-show/${cat.id}`}>
-                  <Button>See More Info</Button>
-                </Link>
+                <NavButton
+                  url={`/cat-show/${cat.id}`}
+                  buttonContent="See More"
+                />
               </CardBody>
             </Card>
           )
