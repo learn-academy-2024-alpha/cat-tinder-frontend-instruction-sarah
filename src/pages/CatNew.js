f4ca5fc88c133f97142c2de23b5350bd0b97a9f9
Sarah Proctor <sproctor950@gmail.com>
2024-04-04 10:25:03 -0700

    :heavy_plus_sign: added react form hook

--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -1,43 +1,86 @@
 import React from "react"
-import { Form, FormGroup, Label, Input, Button, Row, Col } from "reactstrap"
+import { Form, FormGroup, Label, Button, Row, Col } from "reactstrap"
+import { useForm } from "react-hook-form"
+import { useNavigate } from "react-router-dom"
 
-const CatNew = () => {
+const CatNew = ({ createNewCat }) => {
+  const navigate = useNavigate()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const onSubmit = (catData) => {
+    createNewCat(catData)
+    navigate("/cat-index")
+  }
+
   return (
     <div className="new-page-body">
       <h2 className="gochi-hand-regular page-title centering-content">
         Add a New Cat
       </h2>
-      <Form>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Row>
           <Col md={6}>
             <FormGroup>
               <Label for="name">Enter Your Cat's Name</Label>
-              <Input id="name" name="name" placeholder="Name" type="text" />
+              <input
+                id="name"
+                name="name"
+                placeholder="Name"
+                type="text"
+                className="form-control"
+                {...register("name", { required: true })}
+              />
+              {errors.name && <span>Name is required</span>}
             </FormGroup>
           </Col>
           <Col md={6}>
             <FormGroup>
               <Label for="age">Enter Your Cat's Age</Label>
-              <Input
+              <input
                 id="age"
                 name="age"
                 placeholder="Age"
                 type="number"
                 min="0"
+                className="form-control"
+                {...register("age", { required: true })}
               />
+              {errors.age && <span>Age is required</span>}
             </FormGroup>
           </Col>
         </Row>
         <FormGroup>
           <Label for="enjoys">What does your cat enjoy doing?</Label>
-          <Input id="enjoys" name="enjoys" placeholder="Enjoys" type="text" />
+          <input
+            id="enjoys"
+            name="enjoys"
+            placeholder="Enjoys"
+            type="text"
+            className="form-control"
+            {...register("enjoys", { required: true })}
+          />
+          {errors.enjoys && <span>Enjoys is required</span>}
         </FormGroup>
         <FormGroup>
           <Label for="image">Image URL</Label>
-          <Input id="image" name="image" placeholder="Image URL" type="text" />
+          <input
+            id="image"
+            name="image"
+            placeholder="Image URL"
+            type="text"
+            className="form-control"
+            {...register("image", { required: true })}
+          />
+          {errors.image && <span>Image is required</span>}
         </FormGroup>
         <div className="centering-content">
-          <Button>Submit</Button>
+          <Button onClick={handleSubmit} type="submit">
+            Submit
+          </Button>
         </div>
       </Form>
     </div>
