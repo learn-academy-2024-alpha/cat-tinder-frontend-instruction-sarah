d96d24040b6afb44389be1959ee4c2e92418d9d2
Sarah Proctor <sproctor950@gmail.com>
2024-04-05 10:18:28 -0700

    :sparkles: adds requests to Rails API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
@@ -9,15 +9,60 @@ import CatShow from "./pages/CatShow"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import "./App.css"
-import mockCats from "./mockCats"
 
 const App = () => {
-  const [cats, setCats] = useState(mockCats)
-  const createNewCat = (newCat) => {
-    console.log(newCat)
+  const [cats, setCats] = useState([])
+  useEffect(() => {
+    getCats()
+  }, [])
+
+  const getCats = async () => {
+    try {
+      const getResponse = await fetch("http://localhost:3000/cats")
+      if (!getResponse.ok) {
+        throw new Error("Error on the get request for cats")
+      }
+      const getResult = await getResponse.json()
+      setCats(getResult)
+    } catch (error) {
+      alert("Ooops something went wrong:", error.message)
+    }
   }
-  const updateCat = (editCat, id) => {
-    console.log(editCat, id)
+  const createNewCat = async (newCat) => {
+    try {
+      const postResponse = await fetch("http://localhost:3000/cats", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newCat)
+      })
+      if (!postResponse.ok) {
+        throw new Error("Error on the post request for cats")
+      }
+      await postResponse.json()
+      getCats()
+    } catch (error) {
+      alert("Ooops something went wrong:", error.message)
+    }
+  }
+  const updateCat = async (editCat, id) => {
+    try {
+      const patchResponse = await fetch(`http://localhost:3000/cats/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(editCat)
+      })
+      if (!patchResponse.ok) {
+        throw new Error("Error on the patch request for cats")
+      }
+      await patchResponse.json()
+      getCats()
+    } catch (error) {
+      alert("Ooops something went wrong:", error.message)
+    }
   }
   const deleteCat = (id) => {
     console.log(id)
